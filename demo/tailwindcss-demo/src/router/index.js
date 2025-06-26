19aaad110b57f350fe6b3a439c095b9f08e925e5
kyp <2274645898@qq.com>
2025-06-26 23:39:05 +0800

    路由

---
+++ demo/tailwindcss-demo/src/router/index.js
@@ -0,0 +1,12 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+
+const routes = [
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+export default router
