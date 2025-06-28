054c00087d50148b8d3fdb2c1fbc05f98a573471
kyp <2274645898@qq.com>
2025-06-28 18:53:05 +0800

    顶部导航栏适配

--- demo/tailwindcss-demo/src/components/FooterHtml.vue
+++ demo/tailwindcss-demo/src/components/FooterHtml.vue
@@ -2,34 +2,34 @@
     <footer class="bg-gray-800 text-white p-6">
         <section class="flex justify-between items-start flex-col md:flex-row md:justify-normal">
 
-            <div class="text-4xl font-bold flex items-start">
+            <div class="text-3xl font-bold flex items-start">
                 Ceranva
             </div>
             <ul class="w-1/3 md:flex-1 flex justify-between items-center flex-col gap-5 text-ltr">
-                <li><router-link to="/privacy" class="cursor-pointer font-bold">Quick Links</router-link></li>
-                <li><router-link to="/terms" class="cursor-pointer">About Us</router-link></li>
-                <li><router-link to="/contact" class="cursor-pointer">Contact Us</router-link></li>
+                <li><router-link to="/" class="cursor-pointer font-bold">Quick Links</router-link></li>
+                <li><router-link to="/" class="cursor-pointer">About Us</router-link></li>
+                <li><router-link to="/" class="cursor-pointer">Contact Us</router-link></li>
             </ul>
             <ul class="w-1/3 md:flex-1 flex justify-between items-left flex-col gap-5 text-ltr">
-                <li><router-link to="/privacy" class="cursor-pointer font-bold">Contact Info</router-link></li>
+                <li><router-link to="/" class="cursor-pointer font-bold">Contact Info</router-link></li>
                 <li>
-                    <router-link to="/terms" class="cursor-pointer flex items-center gap-2">
+                    <router-link to="/" class="cursor-pointer flex items-center gap-2">
                         <!-- <Message style="width: 1em; height: 1em; margin-right: 8px" /> -->
-                        <span class="w-4 h-4"><el-icon class="w-full"><Message  /></el-icon></span>
-                        <span class="flex-1">sales@ceramic-wx.com</span>
+                        <div class="w-4 h-4"><el-icon class="w-full"><Message  /></el-icon></div>
+                        <div class="flex-1">sales@ceramic-wx.com</div>
                     </router-link>
                 </li>
                 <li>
-                    <router-link to="/terms" class="cursor-pointer flex items-center gap-2">
+                    <router-link to="/" class="cursor-pointer flex items-center gap-2">
                         <!-- <Location class="w-4 h-4"/> -->
-                        <span class="w-4 h-4"><el-icon class="w-full"><Location  /></el-icon></span>
-                        <span class="flex-1">Lisan Village, Yangsanshi Subdistrict, Liling, Hunan</span>
+                        <div class="w-4 h-4"><el-icon class="w-full"><Location  /></el-icon></div>
+                        <div class="flex-1">Lisan Village, Yangsanshi Subdistrict, Liling, Hunan</div>
                     </router-link>
                 </li>
                 <li>
-                    <router-link to="/terms" class="cursor-pointer flex items-center gap-2">
-                        <span class="w-4 h-4"><el-icon class="w-full"><Phone /></el-icon></span>
-                        <span class="flex-1">+86 18627330995</span>
+                    <router-link to="/" class="cursor-pointer flex items-center gap-2">
+                        <div class="w-4 h-4"><el-icon class="w-full"><Phone /></el-icon></div>
+                        <div class="flex-1">+86 18627330995</div>
                     </router-link>
                 </li>
             </ul>
--- demo/tailwindcss-demo/src/components/HeaderHtml.vue
+++ demo/tailwindcss-demo/src/components/HeaderHtml.vue
@@ -2,30 +2,68 @@
     <header class="bg-white w-full h-18 fixed top-0 left-0 right-0 p-3 md:py-4 ">
         <!-- lg:justify-normal ：屏幕宽度 大于等于 1024px 时 justify-normal 生效 -->
         <section class="flex items-center justify-between lg:justify-normal gap-x-20">
-            <div class="ml-10 text-4xl font-bold ">
+            <div class="ml-10 text-3xl font-bold ">
                 <router-link to="/">Ceranva</router-link>
             </div>
-            <ul class="flex items-center justify-between gap-10 text-sm font-bold ">
+            <ul class="hidden sm:flex items-center justify-between gap-10 text-sm font-bold ">
                 <li v-for="item in navLinks" :key="item.id" :class="{'text-orange-400': isActive === item.id}" class="cursor-pointer" @click="isSelect(item.id)">
                     <router-link :to="item.path">{{ item.name }}</router-link>
                 </li>
             </ul>
+            <div @click="expandMenu"  class="sm:hidden ml-auto  flex items-center justify-center w-8 h-8 cursor-pointer">
+               <el-icon class="w-full h-full object-cover"><Menu /></el-icon> 
+            </div>
         </section>
+        <div v-if="isMenuExpanded" class="sm:hidden w-60 absolute top-14 right-3 px-3 bg-white rounded-sm border-1 border-gray-200 shadow-lg z-10">
+            <ul class="flex flex-col items-start justify-start">
+                <li v-for="(item, index) in navLinks" :key="item.id" class="cursor-potinter w-full  py-4" :class="{'border-b border-gray-200': index!==navLinks.length-1}" @click="isSelect(item.id)">
+                    <router-link :to="item.path" class="font-bold" :class="{'text-orange-400': isActive === item.id, }">
+                        <div class="flex justify-between">
+                            <div>{{ item.name }}</div>
+                            <div v-if="item.children"><el-icon><ArrowDown class="w-4 h-4" /></el-icon></div>
+                        </div>
+                    </router-link>
+                    <ul v-if="isChildOpen(item)" class="flex flex-col items-start justify-start gap-2 pt-3 pl-8 text-sm" :class="{ 'hidden': !isActive === item.id }">
+                        <li v-for="child in item.children" :key="child.id" class="cursor-pointer" @click.stop="isSelect(child.id)" >
+                            <router-link :to="child.path" :class="{ 'text-orange-400': isActive === child.id }">{{ child.name }}</router-link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
     </header>
+    <!-- <el-icon><Menu /></el-icon> -->
 </template>
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 const navLinks = [
     { id: 1, name: 'Home', path: '/home' },
-    { id: 2, name: 'Product', path: '/' },
+    { id: 2, name: 'Product', path: '/' ,children: [
+        { id: 2.1, name: 'New bone china', path: '/' },
+        { id: 2.2, name: 'Gold Mug', path: '/' },
+        { id: 2.3, name: 'Lustre Mug', path: '/' },
+        { id: 2.4, name: 'Stoneware Mug', path: '/' },
+        { id: 2.5, name: 'Gift Mug', path: '/' },
+        { id: 2.6, name: 'Dinnerware Set', path: '/' },
+    ]},
     { id: 3, name: 'Blog', path: '/' },
     { id: 4, name: 'About Us', path: '/about' },
     { id: 5, name: 'Contact Us', path: '/' }
 ];
 const isActive = ref(1); // 默认选中 Home
-
 const isSelect = (id) => {
     isActive.value = id;
+}
+
+const isMenuExpanded = ref(false); // 菜单面板是否展开
+const expandMenu = () => {
+    isMenuExpanded.value = !isMenuExpanded.value;
+}
+
+// 子菜单是否打开
+const isChildOpen = (item) => {
+    return item.children && (item.id === isActive.value || item.children.some(child => child.id === isActive.value));
 }
 </script>
 <style scoped>
